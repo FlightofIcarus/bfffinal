e0f995422e6a155854011413b8d4366882d5c282
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2024-03-19 01:40:25 -0300

    refactor(controller): corrige e refatora controller do endpoint do BFF

--- src/controllers/bffController.ts
+++ src/controllers/bffController.ts
@@ -1,17 +1,39 @@
-import "dotenv/config";
 import {Request, Response} from "express"
-import Axios from "axios";
+import { AxiosStatic } from "axios";
 
 
 
-async function bffController(req: Request, res: Response) {
-    const data: {} = req.body;
-    const path: string = String (process.env.API_PATH);
-    const APIReturn = await Axios.post(path, data);
+class BffController{
 
-    res.json(APIReturn);
+    constructor(protected httpManager: AxiosStatic, protected path: string){
+        Object.assign(this, [httpManager, path])
+    }
 
-};
+ bffpost = async (req: Request, res: Response) => {
+    
+    try {
+        
+        const data: {} = req.body;
+        const APIReturn = await this.httpManager.post(this.path, data);
+    
+        console.log(APIReturn);
+        
+    
+        return res.json(APIReturn);
+    
+    }
 
+    catch (error: any) {
+        
+       const APIReturn = {
+        errorCode: error.code,
+        errorMessage: error.message
+       }
 
-export default bffController;
+        return  res.json(APIReturn)
+    }
+}
+
+}
+
+export default BffController;
